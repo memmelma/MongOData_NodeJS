568e61ed9885dfc17964f4624502c3676d438b65
Veyga <marius.memmel@gmx.net>
2018-07-31 14:15:26 +0200

    created generic methods, cleanup, improvement

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var parseSchema = require('mongodb-schema');
 const odataMongo = require('@sealsystems/odata-mongo');
 
 //set mongoDb connection information
+//colName: name of the collection
+//dbName: name of the database
+//url: MongoDb connection url, default: 'mongodb://localhost:27017/'
 var colName = 'product_data';
 var dbName = 'PIM_db';
 var url = 'mongodb://localhost:32768/';
@@ -14,88 +17,102 @@ var url = 'mongodb://localhost:32768/';
 //@sealsystems/odata-mongo
 //https://github.com/sealsystems/node-odata-mongo
 app.get('/data/odata-mongo', odataMongo(), async (req, res) => {
-    let client;
-    let MongoClient = mongodb.MongoClient;
-    client = await MongoClient.connect(url, { useNewUrlParser: true });
-    console.log("Connected to mongodb!");
-    
-    let db = client.db(dbName);
-    db.collection(colName).find(req.mongo.query, req.mongo.queryOptions).toArray((err, docs) => {
-        res.json({
-            '@odata.context': /*req.protocol + '://' + req.get('host') + */'/api/$metadata',
-            value: docs
-        });
-    });
-  });
+    try{
+
+        let client = await connect_to_db();
+        
+        console.log(JSON.stringify(req.mongo));
+
+        get_from_db(res, req, client, '/data/odata-mongo', req.mongo.query, req.mongo.queryOptions)
+
+    }catch(e){
+
+        console.log("Error occured: " + e);
+
+    }
+});
 
 //OData V4 Service modules - MongoDB Connector
 //https://www.npmjs.com/package/odata-v4-mongodb
-app.get("/data/odata-v4-mongodb", async function(req, res) {
+//this one currently ignores select if combined with $filter
+app.get('/data/odata-v4-mongodb', async function(req, res) {
     try{
-        let client;
-        try{
-            //Establish connection with mongoDB instance
-            let MongoClient = mongodb.MongoClient;
-            client = await MongoClient.connect(url, { useNewUrlParser: true });
-            console.log("Connected to mongodb!");
-        }
-        catch(e){
-            res.writeHead(500, {'Content-Type': 'application/json'});
-            res.write("Couldn't connect to database!");
-            res.end();
+        let client = await connect_to_db();
+        let query = JSON.stringify(req.query);
 
-            throw new Error("Couldn't connect to database!" + '\n' + e);
-        }
+        console.log("Select: " + req.query.$select + " - Filter: " + req.query.$filter);
 
-        try{
+        console.log(query);
             
-            console.log(JSON.stringify(req.query));
-            
-            //parser
-            //var parser = require('odata-v4-parser');
-            //console.log(parser.filter(req.query.$filter));
-            
-            //$metadata request
-            if(JSON.stringify(req.query) ==='{"$metadata":""}'){
-                parse_db_shema(res, client);
+            //$metadata request, get db schema
+            if(typeof query === 'undefined'){
+
+                throw new Error('Invalid query');
+
             }
+            else if(query ==='{"$metadata":""}'){
 
-            //Standard query 
+                parse_db_shema(res, client, '/data/odata-v4-mongodb');
+
+            }
+            else if (query === "{}"){
+
+                get_from_db(res, req, client, '/data/odata-v4-mongodb', "{}", )
+
+            }
+            //if query is valid, get data from db
             else{
-                var query = createFilter(req.query.$filter);
 
-                console.log('Filter$ ' + req.query.$filter);
+                let filter = createFilter(req.query.$filter);
+                console.log(filter);
+                get_from_db(res, req, client, '/data/odata-v4-mongodb', filter)
 
-                if(JSON.stringify(req.query) !== '{}' && typeof query === 'undefined'){
-                    throw new Error('Invalid query!' + '\n' + e);
-                }
-
-                get_from_db(res, req, client, query)
             }
-        }
-        catch(e){
-            res.writeHead(404, {'Content-Type': 'application/json'});
-            res.write('Invalid query!');
-            res.end();
-            
-            throw new Error('Invalid query!' + '\n' + e);
-        };
-    }
-    catch(e){
-        console.error(e);
+
+    }catch(e){
+
+        console.log("Error occured: " + e);
+
     }
 });
 
 
-var get_from_db = async (res, req, client, query) => {
+var connect_to_db = async () => {
+    try{
+        //Establish connection with mongoDB instance
+        let MongoClient = mongodb.MongoClient;
+        client = await MongoClient.connect(url, { useNewUrlParser: true });
+
+        console.log("Connected to mongodb!");
+
+        return client;
+    }
+    catch(e){
+
+        res.writeHead(500, {'Content-Type': 'application/json'});
+        res.write("Couldn't connect to database!");
+        res.end();
+
+        throw new Error("Couldn't connect to database!" + '\n' + e);
+    }
+}
+
+var get_from_db = async (res, req, client, route, query, query_options) => {
 
     let db = client.db(dbName);
     let collection = db.collection(colName)
 
-    let tmp = await collection.find(query).toArray();
+    let tmp;
+
+    //console.log(query, query_options);
+
+    if(query_options)
+         tmp = await collection.find(query, query_options).toArray();
+    else
+        tmp = await collection.find(query).toArray();
 
     res.json({
-        '@odata.context': req.protocol + '://' + req.get('host') + '/api',
+        '@odata.context': req.protocol + '://' + req.get('host') + route,
         value: tmp
     });
 
@@ -105,39 +122,28 @@ var get_from_db = async (res, req, client, query) => {
 };
 
 
-var parse_db_shema = async (res, client) => {
+var parse_db_shema = async (res, client, route) => {
     
     let db = client.db(dbName);
     let collection = db.collection(colName)
 
-    // here we are passing in a cursor as the first argument. You can
-    // also pass in a stream or an array of documents directly.
+    //here we are passing in a cursor as the first argument. 
+    //you can also pass in a stream or an array of documents directly.
     parseSchema(collection.find(), /*{semanticTypes: true, storeValues : false},*/ function(err, schema){
         if (err) return console.error(err);
 
         res.json({
-            '@odata.context': /*req.protocol + '://' + req.get('host') + */'/api/$metadata',
+            '@odata.context': req.protocol + '://' + req.get('host') + route + '$metadata',
             value: schema
         });
 
         client.close();
         console.log("Client closed");
 
-    }.bind(res));
+    }.bind(res, req));
 };
 
 var port = process.env.PORT || 8080;
 app.listen(port);
 
 module.exports = app;
-
-/*
-        var r = yield db.collection('inserts').insertOne({a:1});
-        assert.equal(1, r.insertedCount);
-        console.log("Inserted first document");
-
-        // Insert multiple documents
-        var r = yield db.collection('inserts').insertMany([{a:2}, {a:3}]);
-        assert.equal(2, r.insertedCount);
-        console.log("Inserted second document");
-*/
